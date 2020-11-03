9e5eeef0284ddacf1fa8ce9bd6d60a0204719987
Chris Lee <cil5345@gmail.com>
2020-11-03 10:31:02 -0500

    added game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,22 @@
-'use strict';
-console.log(document.querySelector('.message'))
+// 'use strict';
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.message').textContent = 'Correct Number!'
+
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.highscore').textContent = 13;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value)
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    console.log((document.querySelector('.message').textContent = '❌ No Number'));
+  }
+});
